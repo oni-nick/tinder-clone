c623eda76a6d7e960f1787be118513ea97f039cf
Oni-Nick <nicholas00105@gmail.com>
2021-02-13 22:24:05 +0900

    Swipe Button Styling

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import './App.css';
 import Header from "./Header";
 import TinderCards from "./TinderCards";
-
+import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
     // BEM class naming convetion
     <div className="App">
-      <h1>Lets build the MERN Tinder Clone Guys~!!</h1>     
+      {/* <h1>Lets build the MERN Tinder Clone Guys~!!</h1>      */}
     
       {/* Header */}
       <Header />
       {/* Tinder Cards */}
       <TinderCards />
       {/* Swipe Buttons */}
+      <SwipeButtons />
 
     </div>
   );
---
+++ src/SwipeButtons.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './SwipeButtons.css'
+import IconButton from '@material-ui/core/IconButton';
+import ReplayIcon from '@material-ui/icons/Replay';
+import CloseIcon from '@material-ui/icons/Close';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FlashOnIcon from '@material-ui/icons/FlashOn'; 
+
+function SwipeButtons() {
+    return (
+        <div className='swipeButtons'>
+            <IconButton className='swipeButtons__repeat'>
+                <ReplayIcon fontSize='large' />  
+            </IconButton>     
+
+            <IconButton className='swipeButtons__close'>
+                <CloseIcon fontSize='large' />  
+            </IconButton>     
+
+            <IconButton className='swipeButtons__star'>
+                <StarRateIcon fontSize='large' />  
+            </IconButton>      
+
+            <IconButton className='swipeButtons__like'>
+                <FavoriteIcon fontSize='large' />  
+            </IconButton>      
+
+            <IconButton className='swipeButtons__lightning'>
+                <FlashOnIcon fontSize='large' />  
+            </IconButton>            
+            
+        </div>
+    )
+}
+
+export default SwipeButtons
