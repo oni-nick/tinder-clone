213f118be22b2d907015cef62a677a001d403c9e
Oni-Nick <nicholas00105@gmail.com>
2021-02-13 19:52:33 +0900

    Done Header Part

---
+++ src/TinderCards.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TinderCards.css'
+
+function TinderCards() {
+    const [people, setPeople] = sueState([]);
+
+    return (
+        <div className='tinderCards'>
+            
+        </div>
+    )
+}
+
+export default TinderCards
